061b9bc222e7768c83b101443e930bb32f045df9
Kainom <eleuteriokaina@gmail.com>
2024-10-25 19:52:00 -0300

    Routes and Pages has been  added,but, i haven't done anything yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
-
+import { BrowserRouter as Router } from 'react-router-dom'
+import Rotas from "./routes/Routes";
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    <div className='gap-10 border-b  rounded-lg bg-amber-900 mt-20 mx-auto w-1/2 h-64 flex  justify-center items-center'>
-      <p onClick={()=>{setCount(count + 1)}} className='text-amber-500 bg-black p-2.5 cursor-pointer border-b rounded-xl' >Hello World</p>
-      <p className='text-amber-500' >Contador: {count}</p>
-    </div>
-    </>
+    <Router>
+      <Rotas />
+    </Router>
+
   )
 }
 
